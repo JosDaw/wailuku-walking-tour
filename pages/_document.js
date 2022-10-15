e2fbecf5ce69c8b85082ba76ddde726e0f476610
Josie Daw <jdaw1@hotmail.co.uk>
2022-10-16 03:09:20 +0900

    Added Nunito font

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-/**
- * FONTS GO HERE
- */
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400&family=Nanum+Gothic:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
