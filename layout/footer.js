f1a02fd0b6d38b083737574ee8ee800c7a866509
Josie Daw <jdaw1@hotmail.co.uk>
2022-10-16 21:32:18 +0900

    Minor tweaks and favicon

--- layout/footer.js
+++ layout/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 text-base-content bg-primary flex justify-evenly mt-4 flex-wrap">
+    <footer className="footer footer-center p-4 text-base-content bg-primary flex justify-evenly flex-wrap">
       <p className="text-white">
         Copyright © 2022 - All rights reserved, Wailuku Walking Tour
       </p>
