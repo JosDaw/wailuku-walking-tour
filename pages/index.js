bf688b69e378835bee9725797f02729fd62fede8
Yena <yenapaek@gmail.com>
2022-10-15 22:23:38 -0400

    corrected white bar on right screen

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
       backgroundPosition: 'center',
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
-      width: '500px',
-      height: '300px'
+      width: '100%',
+      height: '15rem'
     }
   }
 
